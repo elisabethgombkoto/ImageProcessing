da5b944c9a3ae878e304291fb87e4c3444c9c419
hurrgadev <bernd.orler@students.fhv.at>
2017-11-10 07:24:22 +0100

    Merge branch 'master2' of C:\Users\Bernd\IdeaProjects\WS2017\ImageProcessing\ImageProcessing2 with conflicts.

--- src/main/java/exercise2/PictureSink.java
+++ src/main/java/exercise2/PictureSink.java
@@ -2,8 +2,12 @@ package exercise2;
 
 import calcCentroids.Coordinate;
 import pmp.filter.Sink;
+import pmp.interfaces.Readable;
 
+import java.io.*;
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Bernd on 08.11.2017.
@@ -13,4 +17,104 @@ import java.util.ArrayList;
  * wenn die berechnung statgefunden hat dann soll der result in eine txt file geschrieben werden.
  */
 public class PictureSink extends Sink<ArrayList<Coordinate>> {
+  //expected_coordinates=[(73,77), (110,80), (202,80), (265,79), (330,81), (396,81)]
+
+  private FileWriter _fileWriter;
+  private String _exeptedCoordinates;
+  private  int _tolerance = 5;
+
+  public PictureSink(Readable<ArrayList<Coordinate>> input, String path, FileWriter fileWriter) throws InvalidParameterException{
+    super(input);
+    _exeptedCoordinates = path;
+    _fileWriter = fileWriter;
+  }
+
+  public PictureSink(String path, FileWriter fileWriter)  {
+    _exeptedCoordinates = path;
+    _fileWriter = fileWriter;
+  }
+
+  @Override
+  public void write(ArrayList<Coordinate> actualValues) throws StreamCorruptedException {
+    ArrayList<Coordinate> expectedValues = createExpectedCordinateList(_exceptedMiddelCoordinatePath);
+    StringBuilder stringBuilder = new StringBuilder();
+
+    int i = 1;
+
+    for ( Coordinate cordinate : actualValues) {
+      String s = i++ + ".Point: Centre -> x = " + cordinate._x + " y = " + cordinate._y +
+       ", Tolerance -> ±  " + _tolerance + " in the tolerance range ->  " +
+          isInTolerance(cordinate, expectedValues.get(i-2)) + "\n";
+      stringBuilder.append(s);
+    }
+    try {
+      _fileWriter.write(stringBuilder.toString());
+      _fileWriter.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private boolean isInTolerance(Coordinate actuelCordinate, Coordinate expectedCoordinate) {
+    int differencX = actuelCordinate._x-expectedCoordinate._x;
+    int differencY = actuelCordinate._y-expectedCoordinate._y;
+
+    if((_tolerance> Math.abs(differencX)) &&(_tolerance>Math.abs(differencY))){
+      return true;
+    }
+    return false;
+  }
+
+
+    //TODO Bernd array from file einlesen nicht
+    ArrayList<Coordinate> expected = new ArrayList<Coordinate>();
+
+      Reader reader = null;
+      try
+      {
+          reader = new FileReader( );
+
+          for ( int c; ( c = reader.read() ) != -1; )
+          System.out.print( (char) c );
+      }
+      catch ( IOException e ) {
+          System.err.println( "Fehler beim Lesen der Datei!" );
+      }
+      finally {
+          try { reader.close(); } catch ( Exception e ) { e.printStackTrace(); }
+      }
+
+
+
+
+
+    Coordinate cor1 = new Coordinate(73,77);
+    expected.add(cor1);
+    Coordinate cor2 = new Coordinate(110,80);
+    expected.add(cor2);
+    Coordinate cor3 = new Coordinate(202,80);
+    expected.add(cor3);
+    Coordinate cor4 = new Coordinate(265,79);
+    expected.add(cor4);
+    Coordinate cor5 = new Coordinate(330,81);
+    expected.add(cor5);
+    Coordinate cor6 = new Coordinate(396,81);
+    expected.add(cor6);
+
+    return expected;
+  }
+
+  @Override
+  public void run() {
+    ArrayList<Coordinate> input;
+    try {
+      input = m_Input.read();
+
+      if (input != null) {
+        write(input);
+      }
+    } catch (StreamCorruptedException e) {
+      e.printStackTrace();
+    }
+  }
 }
--- src/main/java/main/RunPush.java
+++ src/main/java/main/RunPush.java
@@ -9,6 +9,7 @@ import pmp.pipes.SimplePipe;
 import javax.media.jai.PlanarImage;
 import javax.media.jai.operator.MedianFilterDescriptor;
 import java.awt.*;
+import java.io.*;
 import java.io.FileNotFoundException;
 
 import java.io.StreamCorruptedException;
@@ -16,6 +17,7 @@ import java.util.ArrayList;
 
 /**
  * Created by Elisabeth on 30.10.2017.
+ *
  */
 public class RunPush {
 
@@ -23,50 +25,54 @@ public class RunPush {
   public static void main(String[] args) throws StreamCorruptedException, FileNotFoundException {
     System.setProperty("com.sun.media.jai.disableMediaLib", "true");
 
+    try {
+      File file = new File(System.getProperty("user.dir")+"\\src\\main\\resources\\resultsPush.txt");
+      FileWriter fileWriter = new FileWriter(file);
 
+      PictureSink pictureSink = new PictureSink(System.getProperty("user.dir")+"\\src\\main\\resources\\expectedCentroids.txt"));
+      SimplePipe <ArrayList<Coordinate>> sp12 = new SimplePipe (pictureSink );
+      CalcCentroidsFilter calcCentroidFilter = new CalcCentroidsFilter(sp12);
+      SimplePipe <PlanarImage> sp11 = new SimplePipe <PlanarImage> (calcCentroidFilter);
 
-    PictureSink pictureSink = new PictureSink();
-    SimplePipe <ArrayList<Coordinate>> sp12 = new SimplePipe (pictureSink );
-    CalcCentroidsFilter calcCentroidFilter = new CalcCentroidsFilter(sp12);
-    SimplePipe <PlanarImage> sp11 = new SimplePipe <PlanarImage> ( calcCentroidFilter );
+      // ImageToFileFilter, Parameterübergabe der dest-directory.
+      ImageToFileFilter imageToFileFilter = new ImageToFileFilter(System.getProperty("user.dir")+"\\src\\main\\resources\\picturePush.jpg",(Writeable<PlanarImage>) sp11);
+      SimplePipe <PlanarImage> sp10 = new SimplePipe <PlanarImage> ((Writeable<PlanarImage>) imageToFileFilter );
 
-    // ImageToFileFilter, Parameterübergabe der dest-directory.
-    ImageToFileFilter imageToFileFilter = new ImageToFileFilter(System.getProperty("user.dir")+"\\src\\main\\resources\\picture.jpg",(Writeable<PlanarImage>) sp11);
-    SimplePipe <PlanarImage> sp10 = new SimplePipe <PlanarImage> ( (Writeable<PlanarImage>) imageToFileFilter );
+      ShowImageFilter showImageFilter5 = new ShowImageFilter((Writeable<PlanarImage>) sp10, "Morphological Transformations Opening-Filter" );
+      SimplePipe <PlanarImage> sp9 = new SimplePipe <PlanarImage> ((Writeable<PlanarImage>) showImageFilter5 );
 
-    ShowImageFilter showImageFilter5 = new ShowImageFilter((Writeable<PlanarImage>) sp10, "Morphological Transformations Opening Filter" );
-    SimplePipe <PlanarImage> sp9 = new SimplePipe <PlanarImage> ( (Writeable<PlanarImage>) showImageFilter5 );
+      //OpeningFilter
+      OpeningFilter openingFilter = new OpeningFilter ((Writeable<PlanarImage>) sp9);
+      SimplePipe <PlanarImage> sp8 = new SimplePipe <PlanarImage> ((Writeable<PlanarImage>) openingFilter );
 
-    //OpeningFilter
-    OpeningFilter openingFilter = new OpeningFilter ((Writeable<PlanarImage>) sp9);
-    SimplePipe <PlanarImage> sp8 = new SimplePipe <PlanarImage> ( (Writeable<PlanarImage>) openingFilter );
+      ShowImageFilter showImageFilter4 = new ShowImageFilter((Writeable<PlanarImage>) sp8, "Median-Filter" );
+      SimplePipe <PlanarImage> sp7 = new SimplePipe <PlanarImage> ((Writeable<PlanarImage>) showImageFilter4 );
 
-    ShowImageFilter showImageFilter4 = new ShowImageFilter((Writeable<PlanarImage>) sp8, "Median-Filter" );
-    SimplePipe <PlanarImage> sp7 = new SimplePipe <PlanarImage> ( (Writeable<PlanarImage>) showImageFilter4 );
+      //MedianFilter, Parameterübergabe Mask Square und size 5.
+      MedianFilter medianFilter = new MedianFilter ((Writeable<PlanarImage>) sp7, MedianFilterDescriptor.MEDIAN_MASK_SQUARE, 5);
+      SimplePipe <PlanarImage> sp6 = new SimplePipe <PlanarImage> ((Writeable<PlanarImage>) medianFilter );
 
-    //MedianFilter, Parameterübergabe Mask Square und size 5.
-    MedianFilter medianFilter = new MedianFilter ((Writeable<PlanarImage>) sp7, MedianFilterDescriptor.MEDIAN_MASK_SQUARE, 5);
-    SimplePipe <PlanarImage> sp6 = new SimplePipe <PlanarImage> ( (Writeable<PlanarImage>) medianFilter );
+      ShowImageFilter showImageFilter3 = new ShowImageFilter((Writeable<PlanarImage>) sp6, "Thresholding-Filter" );
+      SimplePipe <PlanarImage> sp5 = new SimplePipe <PlanarImage> ((Writeable<PlanarImage>) showImageFilter3 );
 
-    ShowImageFilter showImageFilter3 = new ShowImageFilter((Writeable<PlanarImage>) sp6, "Thresholding-Filter" );
-    SimplePipe <PlanarImage> sp5 = new SimplePipe <PlanarImage> ( (Writeable<PlanarImage>) showImageFilter3 );
+      // Tresholdfilter, Parameterübergabe, Parameterübergabe der Schwellenwerte im Konstruktur.
+      TresholdFilter tresholdFilter = new TresholdFilter ((Writeable<PlanarImage>) sp5, 0, 35 , 255);
+      SimplePipe <PlanarImage> sp4 = new SimplePipe <PlanarImage> ((Writeable<PlanarImage>) tresholdFilter );
 
-    // Tresholdfilter, Parameterübergabe, Parameterübergabe der Schwellenwerte im Konstruktur.
-    TresholdFilter tresholdFilter = new TresholdFilter ((Writeable<PlanarImage>) sp5, 0, 35 , 255);
-    SimplePipe <PlanarImage> sp4 = new SimplePipe <PlanarImage> ( (Writeable<PlanarImage>) tresholdFilter );
+      ShowImageFilter showImageFilter2 = new ShowImageFilter((Writeable<PlanarImage>) sp4, " Region of Interest-Filter" );
+      SimplePipe <PlanarImage> sp3 = new SimplePipe <PlanarImage> ((Writeable<PlanarImage>) showImageFilter2 );
 
-    ShowImageFilter showImageFilter2 = new ShowImageFilter((Writeable<PlanarImage>) sp4, " Region of Interest-Filter" );
-    SimplePipe <PlanarImage> sp3 = new SimplePipe <PlanarImage> ( (Writeable<PlanarImage>) showImageFilter2 );
+      // Region of Interest Filter, Parameterübergabe des Bildausschnittes im Konstruktor.
+      RoiFilter roiFilter = new RoiFilter( (Writeable<PlanarImage>) sp3 , new Rectangle(30, 50, 447, 100));
+      SimplePipe <PlanarImage> sp2 = new SimplePipe <PlanarImage> ((Writeable<PlanarImage>) roiFilter );
 
-    // Region of Interest Filter, Parameterübergabe des Bildausschnittes im Konstruktor.
-    RoiFilter roiFilter = new RoiFilter( (Writeable<PlanarImage>) sp3 , new Rectangle(30, 50, 447, 100));
-    SimplePipe <PlanarImage> sp2 = new SimplePipe <PlanarImage> ( (Writeable<PlanarImage>) roiFilter );
+      ShowImageFilter showImageFilter1 = new ShowImageFilter((Writeable<PlanarImage>) sp2, "Original picture" );
+      SimplePipe <PlanarImage> sp1 = new SimplePipe <PlanarImage>((Writeable<PlanarImage>) showImageFilter1);
+      SourcePicture source = new SourcePicture( "loetstellen.jpg", sp1 );
 
-    ShowImageFilter showImageFilter1 = new ShowImageFilter((Writeable<PlanarImage>) sp2, "Original picture" );
-    SimplePipe <PlanarImage> sp1 = new SimplePipe <PlanarImage>( (Writeable<PlanarImage>) showImageFilter1);
-    SourcePicture source = new SourcePicture( "loetstellen.jpg", sp1 );
-
-    source.run();
-
+      source.run();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
