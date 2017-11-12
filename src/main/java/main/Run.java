91c39350614d82dcfd6780dbadf9a15eb2970862
hurrgadev <bernd.orler@students.fhv.at>
2017-11-12 14:48:45 +0100

    Run-Files bearbeitet. exceptedCentroids ausgelesen. Resourcen-Ordner geleert (da kommt nichts rein), Bilder und txt im Hauptverzeichnis.

--- src/main/java/main/Run.java
+++ src/main/java/main/Run.java
@@ -25,7 +25,7 @@ public class Run {
     private void runMethod() throws StreamCorruptedException {
         switch (selected){
             case "a": runPushWithDiameter = new RunPushWithDiameter("\\loetstellen.jpg", "\\expectedCentroids.txt", "\\picturePushWithDiameter.png", "\\resultsPushWithDiameter.txt"); break;
-            case "b": runPullWithDiameter = new RunPullWithDiameter("\\loetstellen.jpg", "\\expectedCentroids.txt", "\\picturePullWithDiameter.png", "\\resultsPushWithDiameter.txt"); break;
+            case "b": runPullWithDiameter = new RunPullWithDiameter("\\loetstellen.jpg", "\\expectedCentroids.txt", "\\picturePullWithDiameter.png", "\\resultsPullWithDiameter.txt"); break;
             case "c": runPush = new RunPush("\\loetstellen.jpg", "\\expectedCentroids.txt", "\\picturePush.png", "\\resultsPush.txt"); break;
             case "d": runPull = new RunPull("\\loetstellen.jpg", "\\expectedCentroids.txt", "\\picturePull.png", "\\resultsPull.txt"); break;
             default: runPushWithDiameter = new RunPushWithDiameter("\\loetstellen.jpg", "\\expectedCentroids.txt", "\\picturePushWithDiameter.png", "\\resultsPushWithDiameter.txt");
